24fbf3c0f8698277b8186410a5121bb55541bde1
heinthant <hthant00chk@gmail.com>
2023-05-15 18:28:49 +0630

    Setup linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
-    def random_greeting 
-        @random_greeting = Greeting.all.sample
-        render json: {greeting: @random_greeting.message}
-    end
+  def random_greeting
+    @random_greeting = Greeting.all.sample
+    render json: { greeting: @random_greeting.message }
+  end
 end

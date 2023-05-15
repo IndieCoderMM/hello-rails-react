a07f9338fb6a1ed4f8b93f2807a041e001ac7b73
heinthant <hthant00chk@gmail.com>
2023-05-15 17:50:30 +0630

    Create homepage

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Greeting() {
+  return <div>Greeting</div>;
+}
+
+export default Greeting;
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root "articles#index"
+  root 'root#index'
 
   get '/random_greeting', to: 'greetings#random_greeting'
 end
